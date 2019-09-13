cdc46054c0bb70441a5e318179dc4e6127583841
전태경 <xorud1544@naver.com>
2019-09-14 01:02:51 +0900

    백준 2869 제출

--- PS/source.cpp
+++ PS/source.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
+	long long a, b, v;
+	cin >> a >> b >> v;
+	long long day = 0;
+
+	if ((v - b) % (a - b) == 0)
+		day = (v - b) / (a - b);
+	else
+		day = (v - b) / (a - b) + 1;
+
+	cout << day << endl;
+	
 	return 0;
 }
