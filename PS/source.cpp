f4b5f5669be1d18fbf4c87dee13eeba7618df6e9
전태경 <xorud1544@naver.com>
2019-09-29 23:16:09 +0900

    백준 14698 제출

--- PS/source.cpp
+++ PS/source.cpp
@@ -1,66 +1,40 @@
+#include <cstdio>
 #include <iostream>
-#include <vector>
 #include <queue>
-#define MAX  50
+#define MOD 1000000007
 
 using namespace std;
-int n;
-vector<int> nodes[MAX + 1];
-bool visited[MAX + 1];
+int T;
 int main()
 {
-	cin >> n;
-	int parent;
-	for (int i = 0; i < n; i++)
+	scanf_s("%d", &T);
+
+	while (T--)
 	{
-		int node;
-		cin >> node;
-		if (node == -1)
+		int n;
+		scanf_s("%d", &n);
+
+		priority_queue<long long> q;
+		long long num, temp;
+
+		while (n--)
 		{
-			parent = i;
-			continue;
+			scanf_s("%lld", &num);
+			q.push(-num);
 		}
 
-		nodes[node].push_back(i);
-		nodes[i].push_back(node);
-	}
-
-	int drop;
-	cin >> drop;
-
-	queue<int> q;
-	q.push(parent);
-	visited[parent] = true;
-
-	if (parent == drop)
-		cout << 0 << endl;
-	else
-	{
-		int ans = 0;
-		while (!q.empty())
+		int ans = 1;
+		while (q.size() > 1)
 		{
-			int next = q.front();
-			q.pop();
+			temp = q.top(); q.pop();
+			temp *= q.top(); q.pop();
 
-			bool isleaf = true;
-			for (int i = 0; i < nodes[next].size(); i++)
-			{
-				int x = nodes[next][i];
-				if (!visited[x])
-				{
-					if (x == drop) continue;
+			ans = temp % MOD * ans % MOD;
 
-					visited[x] = true;
-					q.push(x);
-					isleaf = false;
-				}
-			}
-
-			if (isleaf)
-				ans++;
+			q.push(-temp);
 		}
 
-		cout << ans << endl;
+		printf("%d\n", ans);
 	}
 	return 0;
 }
