ae5128f67562233848ddd513c3c448394bb5f673
전태경 <xorud1544@naver.com>
2019-10-06 21:54:04 +0900

    백준 1717 제출

--- PS/source.cpp
+++ PS/source.cpp
@@ -1,40 +1,57 @@
-#include <cstdio>
 #include <iostream>
-#include <queue>
-#define MOD 1000000007
+#include <cstdio>
+#define MAX		1000000
 
+int arr[MAX + 1];
+
+int n, m;
 using namespace std;
-int T;
+
+int getParent(int a)
+{
+	int parent = arr[a];
+	if (a == parent)
+		return parent;
+	else
+	{
+		return arr[a] = getParent(arr[a]);
+	}	
+}
+
 int main()
 {
-	scanf_s("%d", &T);
-
-	while (T--)
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i <= n; i++)
 	{
-		int n;
-		scanf_s("%d", &n);
+		arr[i] = i;
+	}
 
-		priority_queue<long long> q;
-		long long num, temp;
+	while (m--)
+	{
+		int a, b;
+		int flag;
+		cin >> flag >> a >> b;
 
-		while (n--)
+		if (flag == 0) // 합집합
 		{
-			scanf_s("%lld", &num);
-			q.push(-num);
+			int x = getParent(a);
+			int y = getParent(b);
+			if (x != y) //부모가 다르면 합치자
+			{
+				arr[x] = y;
+			}
 		}
-
-		int ans = 1;
-		while (q.size() > 1)
+		else // 포함?
 		{
-			temp = q.top(); q.pop();
-			temp *= q.top(); q.pop();
-
-			ans = temp % MOD * ans % MOD;
-
-			q.push(-temp);
+			int x = getParent(a);
+			int y = getParent(b);
+			if (x != y)
+				printf("NO\n");
+			else
+				printf("YES\n");
 		}
-
-		printf("%d\n", ans);
 	}
 	return 0;
 }
